3a70b0f80a17ae0f41682d2dd38635cd25d5174f
Elizabeth <e.mora@anoki.it>
2023-10-12 12:07:15 +0200

    environment variables and scripts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
@@ -8,8 +9,8 @@ const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT, (err) => {
     if(err) throw new Error(err);
 
-    console.log('Servidor corriendo en el puerto', 3000);
+    console.log('Servidor corriendo en el puerto', process.env.PORT);
 });
