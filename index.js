0dbf049578923d1aa31db93dc5481d69ec124a46
Elizabeth <e.mora@anoki.it>
2023-10-13 17:27:57 +0200

    socket io

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+//Express app
 const app = express();
+
+//Node Server
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//Socket messages
+io.on('connection', client => {
+    console.log('Connected client');
+
+    client.on('disconnect', () => {
+        console.log('Disconnected client');
+    });
+});
 
 //Public path
 const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, (err) => {
+server.listen(process.env.PORT, (err) => {
     if(err) throw new Error(err);
 
     console.log('Servidor corriendo en el puerto', process.env.PORT);
